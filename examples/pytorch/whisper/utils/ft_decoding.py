0374b96c6ca49e36465aa02d474a6d22458518c3
Utkarsh <49331882+uppalutkarsh@users.noreply.github.com>
2023-04-20 16:09:51 +0530

    Update ft_decoding.py

--- examples/pytorch/whisper/utils/ft_decoding.py
+++ examples/pytorch/whisper/utils/ft_decoding.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2022, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2021-2023, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# %%
+
 import torch
 import torch.nn as nn
 import torch.distributed as dist
@@ -24,8 +24,8 @@ class FTWhisperDecodingWeight(object):
             tensor_para_size,
             pipeline_para_size,
             *,
-            whisper_with_bias=True,
-            mwhisper=True,
+            bart_with_bias=True,
+            mbart=False,
             use_gated_activation=False,
             position_embedding_type=1,
             weight_data_type
@@ -77,11 +77,9 @@ class FTWhisperDecodingWeight(object):
         weight_dict = {}
         qkv_weight_tmp = ["q", "k", "v"] # must respect this order
         qkv_weight_len = 0
-        qkv_bias_tmp = ["q", torch.zeros((model.config.d_model), dtype=torch_weight_dtype, device=model.device), "v"]
-        qkv_bias_len = 1
+        qkv_bias_tmp = ["q", "k", "v"]
+        qkv_bias_len = 0
         for name, param in model.state_dict().items():
-            if name.find(".encoder.") != -1:
-                continue
             name = name.replace("model.", "")
             if param.dim() == 2:
                 param_t = param.transpose(1, 0)
@@ -98,14 +96,14 @@ class FTWhisperDecodingWeight(object):
                         weight_dict[name.partition("self_attn")[0] + "self_attn.qkv_proj.weight"] = qkv_weight
                         qkv_weight_tmp = ["q", "k", "v"]
                         qkv_weight_len = 0
-                elif name.find(".self_attn.q_proj.bias") != -1 or name.find(".self_attn.v_proj.bias") != -1:
-                    qkv_bias_tmp[0 if "q_proj" in name else 2] = param_t # qkv order in weight dict is not guaranteed
+                elif name.find(".self_attn.q_proj.bias") != -1 or name.find(".self_attn.k_proj.bias") != -1 or name.find(".self_attn.v_proj.bias") != -1:
+                    qkv_bias_tmp[0 if "q_proj" in name else 1 if "k_proj" in name else 2] = param_t # qkv order in weight dict is not guaranteed
                     qkv_bias_len += 1
                     if qkv_bias_len == 3:
                         qkv_bias = torch.cat(qkv_bias_tmp, dim=-1)
                         weight_dict[name.partition("self_attn")[0] + "self_attn.qkv_proj.bias"] = qkv_bias
-                        qkv_bias_tmp = ["q", torch.zeros((model.config.d_model), dtype=torch_weight_dtype, device=model.device), "v"]
-                        qkv_bias_len = 1
+                        qkv_bias_tmp = ["q", "k", "v"]
+                        qkv_bias_len = 0
                 else:
                     weight_dict[name] = param_t
             elif name.find("decoder.layernorm_embedding") != -1 or name.find("decoder.layer_norm") != -1 or name.find("final_logits_bias") != -1 or name.find("lm_head") != -1:
@@ -156,19 +154,18 @@ class FTWhisperDecodingWeight(object):
         t = torch.stack([weight_dict["decoder.layers.{}.fc1.weight".format(i)]
                         for i in range(start_layer, end_layer)], 0).contiguous().cuda()
         self.w.append(t.split(t.shape[-1] // self.tensor_para_size, dim=-1)[self.tensor_para_rank].contiguous())
-        # [10] add empty weight for gated activation for now (BART/mBART model by default don't use gated activation)
+        # [10] add empty weight for gated activation for now (WHISPER/mWhisper model by default don't use gated activation)
         self.w.append(torch.empty((1, 1), dtype=torch_weight_dtype).contiguous().cuda())
         # [11]
         t = torch.stack([weight_dict["decoder.layers.{}.fc2.weight".format(i)]
                         for i in range(start_layer, end_layer)], 0).contiguous().cuda()
         self.w.append(t.split(t.shape[1] // self.tensor_para_size, dim=1)[self.tensor_para_rank].contiguous()) 
         # [12] (1) positional embedding table should NOT be transposed, [max position embeddings, hidden size] (2) need to apply offset of 2 for absolute position embeddings in BART/mBART
-        # t = weight_dict["decoder.embed_positions.weight"][2:, :].contiguous().cuda()
-        t = weight_dict["decoder.embed_positions.weight"].contiguous().cuda()
+        t = weight_dict["decoder.embed_positions.weight"][2:, :].contiguous().cuda()
         self.w.append(t)
         # [13] input embedding table should NOT be transposed, [vocab, hidden size]. Directly obtained from raw weight is untransposed
         t = model.get_input_embeddings().weight.contiguous().cuda()
-        # input word embedding may be scaled (mBART), instead of customize this in FT, it's better to modify the embedding loading part in PyT
+        # input word embedding may be scaled (mWhisper), instead of customize this in FT, it's better to modify the embedding loading part in PyT
         embedding_scale = np.sqrt(model.config.d_model) if model.config.scale_embedding else 1.0
         t = t * embedding_scale
         self.w.append(t)
@@ -176,8 +173,7 @@ class FTWhisperDecodingWeight(object):
         t = model.get_output_embeddings().weight.contiguous().cuda() # same as weight_dict["lm_head.weight"].transpose(1, 0).contiguous().cuda() 
         self.w.append(t)
         # [15] LayerNorm after embedding & before transformer block, special in BART/mBART
-        # t = weight_dict["decoder.layernorm_embedding.weight"].contiguous().cuda()
-        t = torch.empty((model.config.d_model), dtype=torch_weight_dtype, device=model.device)
+        t = weight_dict["decoder.layernorm_embedding.weight"].contiguous().cuda()
         self.w.append(t)
         # [16] LayerNorm after transformer block, special in mBART
         if self.mwhisper:
@@ -210,7 +206,7 @@ class FTWhisperDecodingWeight(object):
             t = t.split(t.shape[-1] // self.tensor_para_size, dim=-1)[self.tensor_para_rank].contiguous()
             self.w.append(t)
             # [22]
-            t = torch.stack([torch.zeros((model.config.d_model), dtype=torch_weight_dtype, device=model.device)
+            t = torch.stack([weight_dict["decoder.layers.{}.encoder_attn.k_proj.bias".format(i)]
                             for i in range(start_layer, end_layer)], 0).contiguous().cuda()
             t = t.split(t.shape[-1] // self.tensor_para_size, dim=-1)[self.tensor_para_rank].contiguous()
             self.w.append(t)
@@ -240,8 +236,7 @@ class FTWhisperDecodingWeight(object):
                             for i in range(start_layer, end_layer)], 0).contiguous().cuda()
             self.w.append(t)
             # [29]
-            # t = weight_dict["decoder.layernorm_embedding.bias"].contiguous().cuda()
-            t = torch.empty((model.config.d_model), dtype=torch_weight_dtype, device=model.device)
+            t = weight_dict["decoder.layernorm_embedding.bias"].contiguous().cuda()
             self.w.append(t)
             # [30]
             if self.mwhisper:
@@ -277,21 +272,14 @@ class FTWhisperDecodingWeight(object):
         for i in range(self.real_weights_num):
             self.w[i] = self.w[i].bfloat16()
 
-bad_words_ids = [[1,2,7,8,9,10,14,25,26,27,28,29,31,58,59,60,61,62,63,90,91,92,93,359,503,522,542,873,893,902,918,922,931,1350,1853,1982,2460,2627,3246,3253,3268,3536,3846,3961,4183,4667,6585,6647,7273,9061,9383,10428,10929,11938,12033,12331,12562,13793,14157,14635,15265,15618,16553,16604,18362,18956,20075,21675,22520,26130,26161,26435,28279,29464,31650,32302,32470,36865,42863,47425,49870,50254,50258,50360,50361,50362],
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85]]
-# avoid task translation, only transcribe bc of diff languages causing inconsistencies across chunks
-bad_words_ids[0].append(50259)
-bad_words_ids[1].append(len(bad_words_ids[1]))
 class FTWhisperDecoding(nn.Module):
     def __init__(self, decoding_weight_list, lib_path, head_num, head_size, inter_size,
                  mem_d_model, d_model, num_layer, start_id, end_id, vocab_size, q_scaling=1.0, num_bucket=32,
-                 max_distance=128, tensor_para_size=1, pipeline_para_size=1, whisper_with_bias=True, mwhisper=True, position_embedding_type=1,
-                 activation_type="gelu", layernorm_type="post_layernorm", bad_words_list=None):
+                 max_distance=128, tensor_para_size=1, pipeline_para_size=1, bart_with_bias=True, mbart=False, position_embedding_type=1,
+                 activation_type="gelu", layernorm_type="post_layernorm"):
         super().__init__()
 
-        self.end_id = end_id
         self.use_mpi = dist.is_mpi_available()
-        self.bad_words_list = torch.tensor(bad_words_ids, dtype=torch.int32).to("cuda") if bad_words_list is None else bad_words_list
 
         if self.use_mpi:
             try:
@@ -322,11 +310,11 @@ class FTWhisperDecoding(nn.Module):
                 is_return_output_log_probs, is_return_cum_log_probs, is_return_cross_attentions=False):
         # TODO (bhsueh) Not found an method to put a None Type into op forward function
         # So, the top_k and top_p must be some values now.
-        results = self.decoding.forward2(beam_width, max_seq_len,
+        results = self.decoding.forward(beam_width, max_seq_len,
                                         top_k, top_p, beam_search_diversity_rate,
                                         temperature, len_penalty, repetition_penalty,
                                         random_seed, mem_hidden_states, mem_seq_len,
-                                        is_return_output_log_probs, is_return_cum_log_probs, is_return_cross_attentions, self.bad_words_list)
+                                        is_return_output_log_probs, is_return_cum_log_probs, is_return_cross_attentions)
         return results
 
 
@@ -339,17 +327,12 @@ class FTWhisper(nn.Module):
     def forward(self, input_ids, attention_mask, inputs_embeds, beam_size, max_seq_len,
                 top_k, top_p, beam_search_diversity_rate,
                 temperature=1.0, len_penalty=0.0, repetition_penalty=1.0, random_seed=0,
-                is_return_output_log_probs=False, is_return_cum_log_probs=False, is_return_cross_attentions=False, encoder_outputs=None):
+                is_return_output_log_probs=False, is_return_cum_log_probs=False, is_return_cross_attentions=False):
         
-        if encoder_outputs is None:
-            if self.encoder is None or input_ids is None or attention_mask is None:
-                raise ValueError("input_ids and attention_mask must be provided if encoder_outputs is None")
-            input_ids = input_ids.to("cuda").type(torch.int32)
-            mem_seq_len = torch.sum(attention_mask, dim=1).type(torch.int32).to("cuda")
-            ft_encoder_outputs = self.encoder.forward(input_ids, mem_seq_len, inputs_embeds)
-        else:
-            mem_seq_len = torch.tensor([encoder_outputs.shape[1]]*encoder_outputs.shape[0]).type(torch.int32).to("cuda")
-            ft_encoder_outputs = encoder_outputs
+        input_ids = input_ids.to("cuda").type(torch.int32)
+        mem_seq_len = torch.sum(attention_mask, dim=1).type(torch.int32).to("cuda")
+
+        ft_encoder_outputs = self.encoder.forward(input_ids, mem_seq_len, inputs_embeds)
         results = self.decoding.forward(beam_size,  # optional, can be None
                                         max_seq_len,
                                         top_k,  # optional, can be None
@@ -363,31 +346,15 @@ class FTWhisper(nn.Module):
                                         is_return_cum_log_probs,  # optional, can be None
                                         is_return_cross_attentions,  # optional, can be None
                                         ft_encoder_outputs,
-                                        mem_seq_len,
-                                        )
+                                        mem_seq_len)
         return_dict = {}
-        return_dict['output_ids'] = results.pop(0).reshape([-1, beam_size, max_seq_len]).cpu()
-        return_dict['sequence_lengths'] = results.pop(0).reshape([-1, beam_size]).cpu()
+        return_dict['output_ids'] = results.pop(0).reshape([-1, beam_size, max_seq_len]).cpu().numpy()
+        return_dict['sequence_lengths'] = results.pop(0).reshape([-1, beam_size]).cpu().numpy()
         if is_return_output_log_probs:
-            return_dict['output_log_probs'] = results.pop(0).cpu()
+            return_dict['output_log_probs'] = results.pop(0).cpu().numpy()
         if is_return_cum_log_probs:
-            return_dict['cum_log_probs'] = results.pop(0).cpu()
+            return_dict['cum_log_probs'] = results.pop(0).cpu().numpy()
         if is_return_cross_attentions:
-            return_dict['cross_attentions'] = results.pop(0).cpu()
-
-        return_dict['output_ids'] = pad_ft_eos(return_dict['output_ids'], return_dict['sequence_lengths'], self.decoding.end_id)
+            return_dict['cross_attentions'] = results.pop(0).cpu().numpy()
+            
         return return_dict
-
-def pad_ft_eos(output_ids, sequence_lengths, eos_token):
-    """
-    pad the output_ids[sequence_lengths:] with eos_token
-
-    items: tensor (batch_size, seq_len)
-
-    out: tensor (batch_size, seq_len)
-    """
-    batch_size, beam_size, seq_len = output_ids.shape
-    for i in range(batch_size):
-        for j in range(beam_size):
-            output_ids[i, j, sequence_lengths[i]:] = eos_token
-    return output_ids
